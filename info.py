ddb2f719a1b176a82d6e66064752ae098706c10c
Sunaif_adkar <80641581+sunaif-adkar2@users.noreply.github.com>
2021-08-29 01:27:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,13 +5,11 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
-MAX_RESULTS = int(environ.get('MAX_RESULTS', 10))
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
@@ -22,18 +20,27 @@ auth_users = [int(user) if id_pattern.search(user) else user for user in environ
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
-
+AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
+TUTORIAL = "https://youtu.be/FCU_XJmyG_U"
 # MongoDB information
 DATABASE_URI = environ['DATABASE_URI']
 DATABASE_NAME = environ['DATABASE_NAME']
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
-START_MSG = """
-**Hi, I'm Media Search bot**
-
-Here you can search files in inline mode. Just press following buttons and start searching.
+default_start_msg = """
+**Hi, I'm Media Search Bot or ypu can call me as Auto-Filter Bot**
+Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
 """
+START_MSG = environ.get('START_MSG', default_start_msg)
 
-SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
-INVITE_MSG = environ.get('INVITE_MSG', 'Please join @.... to use this bot')
+FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
+OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
+if FILE_CAPTION.strip() == "":
+    CUSTOM_FILE_CAPTION=None
+else:
+    CUSTOM_FILE_CAPTION=FILE_CAPTION
+if OMDB_API_KEY.strip() == "":
+    API_KEY=None
+else:
+    API_KEY=OMDB_API_KEY
